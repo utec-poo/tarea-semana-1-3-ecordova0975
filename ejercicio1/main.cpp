7dd354bc4e43d20615b37bdbe76b6b67a55b9d66
E.Córdova <52300931+ecordova0975@users.noreply.github.com>
2019-09-15 14:30:38 -0500

    Update main.cpp

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main() {
 
-entero base=0, entero altura=0;
+entero base=0, entero altura=0, int area=0;
 cout << "\ningrese la base:";
 cin >> base;
 cout << "\ningrese la altura:";
 cin >> altura;
+area = x*y;
+cout<<"El area es: "<<area<<"."
 
 
 // ToDO
