3e5d424c0beb2ac120fe66537769360d199ccb59
E.Córdova <52300931+ecordova0975@users.noreply.github.com>
2019-09-15 14:26:55 -0500

    Update main.cpp

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -2,16 +2,23 @@
 #include "Tipos.h"
 using namespace std;
 
-int main() {
+int Area();
 
-entero base=0, entero altura=0;
+int main(int,int) {
+
+entero base=0, entero altura=0, entero area=0;
 cout << "\ningrese la base:";
 cin >> base;
 cout << "\ningrese la altura:";
 cin >> altura;
+area = Area(base,altura);
+cout << "\nEl area es: "<<area<<"."
 
 
 // ToDO
 
   return 0;
 }
+
+int Area(int x,int y){
+  return x*y;
