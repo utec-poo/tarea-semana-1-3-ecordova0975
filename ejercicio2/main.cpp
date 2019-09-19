747b2d3eb086a3ec3a7da11908a9194c6a58b054
E.Córdova <52300931+ecordova0975@users.noreply.github.com>
2019-09-18 19:30:34 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
+int opc=0;
+double precio=0;
+void Menu();
+void Descuento();
 
-
-// ToDO
-
-  return 0;
+void Menu(){
+    cout<<"\t.:Menu:."<<endl;
+    cout<<"1. Super Vip"<<endl;
+    cout<<"2. Vip"<<endl;
+    cout<<"3. Preferencial"<<endl;
+    cout<<"4. General"<<endl;
+    do{
+        cout<<"Cual desea comprar?: "<<endl;cin>>opc;
+    }while(opc<0 or opc>4);
+    switch(opc){
+        default: cout<<"Ingresa una opción valida";
+        case 1: precio = 212; break;
+        case 2: precio = 170; break;
+        case 3: precio = 136; break;
+        case 4: precio = 59; break;
+    }
+    cout<<"El precio por la opcion "<<opc<<" es de: S/ "<<precio;
 }
+
+void Descuento(){
+    char dsc;
+    do {
+        cout << "\nDesea aplicar el descuento? (s/n)";cin>>dsc;
+    }while(dsc!='s' && dsc!='S' && dsc!='n' && dsc!='N');
+    if(dsc=='s' or dsc=='S'){
+        precio*=0.8;
+    }
+}
+
+int main(){
+    Menu();
+    Descuento();
+    cout<<"El precio final es de: S/ "<<precio<<".";
+    return 0;
+}
+
